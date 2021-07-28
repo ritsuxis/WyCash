6674923f8c8adb37729bb83bfc0ef85575d2ad58
ritsuxis <ritsuxis@gmail.com>
2021-07-28 16:28:55 +0900

    MoneyTestからDollarへの参照をけした

--- MoneyTest/MoneyTest.cs
+++ MoneyTest/MoneyTest.cs
@@ -10,18 +10,18 @@ namespace MoneyTest
         public void TestMultiplication()
         {
             Money.Money five = Money.Money.MakeDollar(5);
-            Assert.IsTrue(new Dollar(10).Equals(five.Times(2)));
-            Assert.IsTrue(new Dollar(15).Equals(five.Times(3)));
+            Assert.IsTrue(Money.Money.MakeDollar(10).Equals(five.Times(2)));
+            Assert.IsTrue(Money.Money.MakeDollar(15).Equals(five.Times(3)));
         }
 
         [TestMethod]
         public void TestEquality()
         {
-            Assert.IsTrue(new Dollar(5).EqualAmount(new Dollar(5)));
-            Assert.IsFalse(new Dollar(5).EqualAmount(new Dollar(6)));
+            Assert.IsTrue(Money.Money.MakeDollar(5).EqualAmount(Money.Money.MakeDollar(5)));
+            Assert.IsFalse(Money.Money.MakeDollar(5).EqualAmount(Money.Money.MakeDollar(6)));
             Assert.IsTrue(new Franc(5).EqualAmount(new Franc(5)));
             Assert.IsFalse(new Franc(5).EqualAmount(new Franc(6)));
-            Assert.IsFalse(new Franc(5).EqualAmount(new Dollar(5)));
+            Assert.IsFalse(new Franc(5).EqualAmount(Money.Money.MakeDollar(5)));
         }
 
         [TestMethod]
